9965fdb24af7b3eb078de630cc413aa245dbde5d
Willy Kim <singsok@gmail.com>
2019-08-22 20:35:13 +0900

    My First App, First Commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^y@rbojvo^h*719!aq6-vk!x4^(%0uk2qh^=@2)r$8sswymiq^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
